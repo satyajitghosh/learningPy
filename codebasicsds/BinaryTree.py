44077213796c34f45bee8f6f604d22cd8d0089e9
Satyajit Ghosh <satyajitghosh@Satyajits-MacBook-Air.local>
2021-12-27 02:27:23 +0530

    EOS Commit

---
+++ codebasicsds/BinaryTree.py
@@ -0,0 +1,62 @@
+###Binary Tree/Binary Search Tree###
+# A node in a binary tree cannot have more than 2 nodes.
+# In a binary tree the left child has to be lower than the parent and the right child has to have a value higher than that of the parent.
+
+class BinarySearchTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self,data):
+        if self.data == data:
+            return
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return(elements)
+
+    def search(self,val):
+        if val == self.data:
+            return True
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+        else:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    for element in elements:
+        root.add_child(element)
+    return(root)
+
+if __name__ == '__main__':
+    numbers = [17,4,1,20,9,23,18,34]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.in_order_traversal())
+    print(numbers_tree.search(20))
